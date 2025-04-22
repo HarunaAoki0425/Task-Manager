97d77e16cceaf1712c75cedf81c68cc2ca0f0c33
HarunaAoki0425 <haruna.aoki@pathoslogos.co.jp>
2025-04-22 15:34:20 +0900

    初回

---
+++ src/environment/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebase: {
+     apiKey: "AIzaSyA4dkYXIplXCCGZhgj4YHeDZKGJ6OZQ4rk",
+     authDomain: "task-manager-f7b3c.firebaseapp.com",
+     projectId: "task-manager-f7b3c",
+     storageBucket: "task-manager-f7b3c.firebasestorage.app",
+     messagingSenderId: "888120882480",
+     appId: "1:888120882480:web:ef29b3af6bed3e001644f5"
+  }
+};

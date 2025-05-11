e27aacd14c833ecc30c9ed64c0f17cd818b99237
HarunaAoki0425 <haruna.aoki@pathoslogos.co.jp>
2025-05-11 20:15:03 +0900

    ホスティング

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
+// src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: '/api'  // 本番環境のAPIエンドポイント
-}; 
+  apiUrl: '/api',  // 本番環境のAPIエンドポイント
+  firebase: {
+    apiKey: "AIzaSyA4dkYXIplXCCGZhgj4YHeDZKGJ6OZQ4rk",
+    authDomain: "task-manager-f7b3c.firebaseapp.com",
+    projectId: "task-manager-f7b3c",
+    storageBucket: "task-manager-f7b3c.appspot.com",
+    messagingSenderId: "888120882480",
+    appId: "1:888120882480:web:ef29b3af6bed3e001644f5"
+  }
+};
